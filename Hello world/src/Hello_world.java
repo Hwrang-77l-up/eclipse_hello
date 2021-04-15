e2f457a8e4ef23f118c60ea9eded8180f0e66b9b
hyeop27 <kimj7979@gmail.com>
2021-04-15 10:45:41 +0900

    added 'his world'

--- Hello world/src/Hello_world.java
+++ Hello world/src/Hello_world.java
@@ -4,6 +4,7 @@ public class Hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello your world");
+		System.out.println("hello his world");
 
 	}
 
